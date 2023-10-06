a11fa9abde89989a0bc4a4efb36278363d614953
praveen.mada <praveen.mada@dish.com>
2023-10-06 15:38:50 -0600

    bugfixes for style issues

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -3,17 +3,36 @@ import { title } from '../components/primitives';
 import { fetchGraphQL } from './api/api';
 
 export default function PlaygroundPage() {
+  /**
+   * useState hook to manage the query and response states of the PlaygroundPage component.
+   * query: string - the GraphQL query entered by the user.
+   * setQuery: function - updates the query state.
+   * response: string - the response received from the GraphQL API.
+   * setResponse: function - updates the response state.
+   */
+
   const [query, setQuery] = useState('');
   const [response, setResponse] = useState('');
 
+  /**
+   * Event handler function that updates the query state when the user types in the query textarea.
+   * @param event - the change event triggered by the user typing in the query textarea.
+   */
   const handleQueryChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setQuery(event.target.value);
   };
 
+  /**
+   * Event handler function that updates the response state when the user types in the response textarea.
+   * @param event - the change event triggered by the user typing in the response textarea.
+   */
   const handleResponseChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setResponse(event.target.value);
   };
 
+  /**
+   * Event handler function that makes an API call to fetch data from the GraphQL API and updates the response state.
+   */
   const handleApiCall = async () => {
     try {
       const data = await fetchGraphQL(query);
