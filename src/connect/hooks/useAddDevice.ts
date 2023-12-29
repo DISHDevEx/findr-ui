0a8719ffd6a6226bcabf9193da1dddae24882129
lauren.mieczkowski <lauren.mieczkowski@dish.com>
2023-12-29 11:15:19 -0700

    changed base URL

--- src/connect/hooks/useAddDevice.ts
+++ src/connect/hooks/useAddDevice.ts
@@ -5,14 +5,14 @@ import { Device } from "../types/device";
 import { mock } from "../../mocks/server";
 
 const findrapi = axios.create({
-  baseURL: 'http://afd0e01f0d9594dc5adbd0350e4454c7-1214440607.us-east-1.elb.amazonaws.com',
+  baseURL: "http://afd0e01f0d9594dc5adbd0350e4454c7-1214440607.us-east-1.elb.amazonaws.com/oracle",
   timeout: 1000,
   headers: {'Content-Type': 'application/json'}
 });
 
 const addDevice = async (device: Device): Promise<Device> => {
   mock.restore();
-  const { data } = await findrapi.post("/oracle", device);
+  const { data } = await findrapi.post("", device);
   return data;
 };
 
