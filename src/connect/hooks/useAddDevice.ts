2ebf61da9966dc687bb2bded291557166f4217ef
lauren.mieczkowski <lauren.mieczkowski@dish.com>
2024-01-02 12:12:09 -0700

    switch to https

--- src/connect/hooks/useAddDevice.ts
+++ src/connect/hooks/useAddDevice.ts
@@ -5,7 +5,7 @@ import { Device } from "../types/device";
 import { mock } from "../../mocks/server";
 
 const findrapi = axios.create({
-  baseURL: "http://afd0e01f0d9594dc5adbd0350e4454c7-1214440607.us-east-1.elb.amazonaws.com:9000",
+  baseURL: "https://afd0e01f0d9594dc5adbd0350e4454c7-1214440607.us-east-1.elb.amazonaws.com:9000",
   timeout: 1000,
   headers: {'Content-Type': 'application/json'}
 });
