c747824f66d4ec68f767ed7ba3490ddf47781dc7
lauren.mieczkowski <lauren.mieczkowski@dish.com>
2024-01-03 15:10:45 -0700

    added Access-Control-Allow-Origin header

--- src/connect/components/DeviceForm.tsx
+++ src/connect/components/DeviceForm.tsx
@@ -75,7 +75,7 @@ const DeviceForm = ({
     const findrapi = axios.create({
       baseURL: 'http://afd0e01f0d9594dc5adbd0350e4454c7-1214440607.us-east-1.elb.amazonaws.com:9000',
       timeout: 2000,
-      headers: {'Content-Type': 'application/json'}
+      headers: {'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*"}
     });
   
   
